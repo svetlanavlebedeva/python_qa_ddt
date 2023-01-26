12276bf65ba981b0315a422971c8fdf4f16127e8
Svetlana <svetlana.vl.koroleva@gmail.com>
2023-01-26 19:06:33 +0400

    Update examples

--- marks/test_example.py
+++ marks/test_example.py
@@ -1,24 +1,32 @@
 import pytest
 
+run = False
 
-@pytest.mark.regress
+
+@pytest.mark.smoke
 @pytest.mark.skip(reason="JIRA-123")
 def test_example_one():
     assert 0
 
 
 @pytest.mark.regress
-@pytest.mark.xfail(run=False)
+@pytest.mark.skipif("not run")
 def test_example_two():
+    assert 0
+
+
+@pytest.mark.regress
+@pytest.mark.xfail(run=False)
+def test_example_three():
     assert True
 
 
 @pytest.mark.regress
 @pytest.mark.xfail(strict=True)
-def test_example_three():
+def test_example_four():
     assert True
 
 
 @pytest.mark.auth
-def test_example_four():
+def test_example_five():
     pass
